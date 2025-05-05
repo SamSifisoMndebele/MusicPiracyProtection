4b624d4c7e0757dfad49359e3881e6db7facad18
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-05 21:55:40 +0200

    Add Koin dependency injection setup
    Introduced Koin DI framework integration in the project. Configured Koin with a MongoDB connection and AuthRepository as singletons to streamline dependency management. This setup enhances modularity and simplifies testing.

---
+++ src/main/kotlin/plugins/Koin.kt
@@ -0,0 +1,18 @@
+package ul.group14.plugins
+
+import io.ktor.server.application.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+import ul.group14.repositories.AuthRepository
+import ul.group14.repositories.connectToMongoDB
+
+fun Application.configureKoin() {
+    install(Koin) {
+        slf4jLogger()
+        modules(module {
+            single(createdAtStart = true) { connectToMongoDB() }
+            single(createdAtStart = true) { AuthRepository(get()) }
+        })
+    }
+}
