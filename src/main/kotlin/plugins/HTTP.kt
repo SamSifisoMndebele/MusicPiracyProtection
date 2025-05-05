cc655bbbe95bfab603affd63244de98c329162ee
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-05 21:55:55 +0200

    Remove Koin setup and adjust CORS configuration.
    The Koin dependency and associated configuration were removed from `Frameworks.kt` as it is no longer needed. Updated `HTTP.kt` to expand CORS support to additional hosts, improving development and testing flexibility.

--- src/main/kotlin/plugins/HTTP.kt
+++ src/main/kotlin/plugins/HTTP.kt
@@ -6,7 +6,6 @@ import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
-    install(Compression)
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Get)
@@ -16,5 +15,10 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowHost("musicpiracyprotection.com")
+        allowHost("localhost")
+        allowHost("localhost:8081")
+        allowHost("127.0.0.1")
+        allowHost("127.0.0.1:8081")
     }
 }
