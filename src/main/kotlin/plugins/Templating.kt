87bdbfc992c696291e64f1533e14ff987a67c75f
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-05 21:57:10 +0200

    Refactor templating module and enhance password reset UI
    Replaced legacy routes and redundant CSS DSL with Thymeleaf template rendering. Integrated authentication for user-specific pages and added a password reset form with modern styling and JavaScript assets for better UX.

--- src/main/kotlin/plugins/Templating.kt
+++ src/main/kotlin/plugins/Templating.kt
@@ -1,68 +1,112 @@
 package ul.group14.plugins
 
-import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.principal
 import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
-import kotlinx.css.*
 import kotlinx.html.*
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+import ul.group14.data.model.response.User
 
 fun Application.configureTemplating() {
     install(Thymeleaf) {
         setTemplateResolver(ClassLoaderTemplateResolver().apply {
-            prefix = "templates/thymeleaf/"
+            prefix = "thymeleaf/"
             suffix = ".html"
             characterEncoding = "utf-8"
         })
     }
     routing {
-        get("/html-dsl") {
-            call.respondHtml {
-                body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
-                    }
+        authenticate {
+            get {
+                val principal = call.principal<User>() ?: run {
+                    call.respondRedirect("/login", permanent = true)
+                    return@get
                 }
-            }
-        }
-        get("/styles.css") {
-            call.respondCss {
-                body {
-                    backgroundColor = Color.darkBlue
-                    margin(0.px)
-                }
-                rule("h1.page-title") {
-                    color = Color.white
-                }
+                call.respond(ThymeleafContent(
+                    "index",
+                    mapOf("user" to principal)
+                ))
             }
         }
 
-        get("/html-css-dsl") {
+
+        get("/password") {
             call.respondHtml {
                 head {
-                    link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+                    title { +"Password Reset" }
+                    meta { charset = "UTF-8" }
+                    meta { name = "viewport"; content = "width=device-width, initial-scale=1" }
+                    link(rel = "stylesheet", href = "/vendor/bootstrap/css/bootstrap.min.css", type = "text/css")
+                    link(
+                        rel = "stylesheet",
+                        href = "/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
+                        type = "text/css"
+                    )
+                    link(rel = "stylesheet", href = "/vendor/animate/animate.css", type = "text/css")
+                    link(
+                        rel = "stylesheet",
+                        href = "/vendor/css-hamburgers/hamburgers.min.css",
+                        type = "text/css"
+                    )
+                    link(rel = "stylesheet", href = "/vendor/select2/select2.min.css", type = "text/css")
+                    link(rel = "stylesheet", href = "/css/util.css", type = "text/css")
+                    link(rel = "stylesheet", href = "/css/auth.css", type = "text/css")
                 }
                 body {
-                    h1(classes = "page-title") {
-                        +"Hello from Ktor!"
+                    div("limiter") {
+                        div("container-login100") {
+                            div("wrap-login100") {
+                                div("login100-pic js-tilt") {
+                                    attributes["data-tilt"] = "true"
+                                    h1("login100-pic-title") { +"Music Piracy Protection System" }
+                                    img(src = "/images/background.JPG", alt = "IMG")
+                                }
+                                form(
+                                    action = "/password",
+                                    method = FormMethod.post,
+                                    classes = "login100-form validate-form"
+                                ) {
+                                    span("login100-form-title") { +"Password Reset" }
+                                    div("wrap-input100 validate-input") {
+                                        attributes["data-validate"] = "Valid email is required: ex@abc.xyz"
+                                        input(type = InputType.email, classes = "input100") {
+                                            name = "email"
+                                            placeholder = "Email"
+                                        }
+                                        span("focus-input100")
+                                        span("symbol-input100") {
+                                            i("fa fa-envelope") { attributes["aria-hidden"] = "true" }
+                                        }
+                                    }
+                                    div("container-login100-form-btn") {
+                                        button(type = ButtonType.submit, classes = "login100-form-btn") {
+                                            +"Send Reset Email"
+                                        }
+                                    }
+                                    div("text-center p-t-16") {
+                                        a("/login", classes = "txt2") {
+                                            +"Login to your Account"
+                                            i("fa fa-long-arrow-right m-l-5") { attributes["aria-hidden"] = "true" }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
+                    script { src = "/vendor/jquery/jquery-3.2.1.min.js" }
+                    script { src = "/vendor/bootstrap/js/popper.js" }
+                    script { src = "/vendor/bootstrap/js/bootstrap.min.js" }
+                    script { src = "/vendor/select2/select2.min.js" }
+                    script { src = "/vendor/tilt/tilt.jquery.min.js" }
+                    script { +"$('.js-tilt').tilt({scale: 1.1})" }
+                    script { src = "/js/password.js" }
                 }
             }
         }
-        get("/html-thymeleaf") {
-            call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
-        }
+
     }
 }
-
-suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
-    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
-}
-
-data class ThymeleafUser(val id: Int, val name: String)
