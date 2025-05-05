52eb0ea63dfa7650eb0a4cf1743ae827428e6050
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-05 21:56:51 +0200

    Add ContentNegotiation and Compression plugins
    Integrated ContentNegotiation with JSON configuration to improve handling of JSON responses. Enabled Compression to optimize response sizes, enhancing overall application performance. Simplified the routing path for JSON responses.

--- src/main/kotlin/plugins/Serialization.kt
+++ src/main/kotlin/plugins/Serialization.kt
@@ -1,13 +1,22 @@
 package ul.group14.plugins
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.plugins.compression.Compression
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
+    install(Compression)
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+        })
+    }
     routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        get("/json") { call.respond(mapOf("hello" to "world")) }
     }
 }
