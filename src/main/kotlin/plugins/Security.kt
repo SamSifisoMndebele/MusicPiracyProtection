170ac9749bf5645bcd5545606aee4c57173fe630
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-05 21:57:23 +0200

    Implement session-based authentication and login/register routes
    Replaced basic and form authentication with session-based auth. Added login and register routes with corresponding pages and error handling. Integrated `AuthRepository` for user authentication and session management.

--- src/main/kotlin/plugins/Security.kt
+++ src/main/kotlin/plugins/Security.kt
@@ -1,82 +1,107 @@
 package ul.group14.plugins
 
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.html.*
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import org.koin.ktor.ext.inject
+import ul.group14.data.model.response.User
+import ul.group14.pages.auth.loginPage
+import ul.group14.pages.auth.registerPage
+import ul.group14.repositories.AuthRepository
+import ul.group14.repositories.AuthRepository.Companion.PasswordException
+import ul.group14.repositories.AuthRepository.Companion.UserNotFoundException
+import kotlin.time.Duration.Companion.hours
 
 fun Application.configureSecurity() {
-//    val mongoDatabase = connectToMongoDB()
-    authentication {
-        basic(name = "myauth1") {
-            realm = "Access to the Server"
-            validate { credentials ->
-                if (credentials.name == "jetbrains" && credentials.password == "foobar") {
-                    UserIdPrincipal(credentials.name)
-                } else {
-                    null
-                }
-            }
+    val authRepository by inject<AuthRepository>()
+    install(Sessions) {
+        cookie<User>("user_session") {
+//            cookie.path = "/"
+//            cookie.secure = false
+            cookie.maxAge = 8.hours
         }
-
-        form {
-            userParamName = "email"
-            passwordParamName = "password"
-            validate { credentials ->
-                if (credentials.name == "jetbrains" && credentials.password == "foobar") {
-                    UserIdPrincipal(credentials.name)
+    }
+    authentication {
+        session<User> {
+            validate { session ->
+                if(session.email.isNotBlank()) {
+                    session
                 } else {
                     null
                 }
             }
             challenge {
-                call.respond(HttpStatusCode.Unauthorized, "Credentials are not valid")
+                call.respondRedirect("/login")
             }
         }
     }
-//    authentication {
-//        oauth("auth-oauth-google") {
-//            urlProvider = { "http://localhost:8080/callback" }
-//            providerLookup = {
-//                OAuthServerSettings.OAuth2ServerSettings(
-//                    name = "google",
-//                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-//                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-//                    requestMethod = HttpMethod.Post,
-//                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-//                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-//                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
-//                )
-//            }
-//            client = HttpClient(Apache)
-//        }
-//    }
     routing {
-        authenticate("myauth1") {
-            get("/protected/route/basic") {
-                val principal = call.principal<UserIdPrincipal>()
-                call.respondText("Hello ${principal?.name}")
+        get("/login") {
+            call.respondHtml {
+                loginPage()
             }
         }
-        authenticate {
-            get("/protected/route/form") {
-                val principal = call.principal<UserIdPrincipal>()
-                call.respondText("Hello ${principal?.name}")
+        post("/login") {
+            val parameters = call.receiveParameters()
+            val email = parameters["email"].toString()
+            val password = parameters["password"].toString()
+            try {
+                val user = authRepository.login(email, password)
+                call.sessions.set(user)
+                println("$user logged in successfully")
+                call.respondRedirect("/", permanent = true)
+            } catch (e: UserNotFoundException) {
+                call.respondHtml {
+                    loginPage(
+                        email = email,
+                        password = password,
+                        emailError = e.localizedMessage
+                    )
+                }
+            } catch (e: PasswordException) {
+                call.respondHtml {
+                    loginPage(
+                        email = email,
+                        password = password,
+                        passwordError = e.localizedMessage
+                    )
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respondHtml {
+                    loginPage(
+                        email = email,
+                        password = password,
+                    )
+                }
             }
         }
-//        authenticate("auth-oauth-google") {
-//            get("login") {
-//                call.respondRedirect("/callback")
-//            }
-//
-//            get("/callback") {
-//                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-//                call.sessions.set(UserSession(principal?.accessToken.toString()))
-//                call.respondRedirect("/hello")
-//            }
-//        }
+
+        get("/register") {
+            call.respondHtml {
+                registerPage()
+            }
+        }
+        post("/register") {
+            val parameters = call.receiveParameters()
+            val email = parameters["email"].toString()
+            val password = parameters["password"].toString()
+            val passwordConfirm = parameters["password_confirm"].toString()
+            if (password != passwordConfirm) {
+                call.respondHtml {
+                    registerPage(
+                        email = email,
+                        password = password,
+                        passwordConfirm = passwordConfirm,
+                        passwordConfirmError = "Passwords do not match."
+                    )
+                }
+            }
+            call.respond("Register Response")
+        }
     }
 }
-
-//class UserSession(accessToken: String)
